8d14ebe09c014324113cc55d8bbcda54ac5668b7
SOunit <workbookwork7@gmail.com>
2022-04-13 16:03:52 -0700

    add sample

--- 02_react_typescript/src/components/App.tsx
+++ 02_react_typescript/src/components/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div>App works</div>;
+import React from "react";
+import { User } from "../models/Model";
+import { AuthService } from "../services/AuthService";
+import { Login } from "./Login";
+
+interface AppState {
+  user: User | undefined;
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return (
+      <div>
+        App from class works!!!
+        <Login authService={this.authService} />
+      </div>
+    );
+  }
+}
