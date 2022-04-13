b6b0eaefa1cdfb23abf2a11e35bad12bd1683c0f
SOunit <workbookwork7@gmail.com>
2022-04-13 16:16:48 -0700

    add login code

--- 02_react_typescript/src/components/Login.tsx
+++ 02_react_typescript/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { AuthService } from "../services/AuthService";
 
 interface LoginProps {
@@ -12,6 +12,10 @@ interface LoginState {
   loginSuccessful: boolean;
 }
 
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
 export class Login extends React.Component<LoginProps, LoginState> {
   state: LoginState = {
     userName: "",
@@ -20,14 +24,44 @@ export class Login extends React.Component<LoginProps, LoginState> {
     loginSuccessful: false,
   };
 
+  private setUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+    console.log("Setting user name to", event.target.value);
+  }
+  private setPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    );
+
+    if (result) {
+      console.log(result);
+    } else {
+      console.log("wrong login");
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Please login</h2>
-        <form action="">
-          <input type="text" value={this.state.userName} />
+        <form action="" onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={(event) => this.setUserName(event)}
+          />
           <br />
-          <input type="password" value={this.state.password} />
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={(event) => this.setPassword(event)}
+          />
           <br />
           <input type="submit" value={"Login"} />
         </form>
