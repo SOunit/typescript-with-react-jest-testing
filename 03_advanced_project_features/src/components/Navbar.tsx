0b21e87d839232fd12e663393fe03d2e9623aa9f
SOunit <workbookwork7@gmail.com>
2022-04-13 16:46:59 -0700

    add navbar component

---
+++ 03_advanced_project_features/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { User } from "../models/Model";
+
+export class Navbar extends React.Component<{ user: User | undefined }> {
+  render() {
+    let loginLogOut: any;
+    if (this.props.user) {
+      loginLogOut = <Link to="/logout">{this.props.user.userName}</Link>;
+    } else {
+      loginLogOut = <Link to="/login">login</Link>;
+    }
+
+    return (
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {loginLogOut}
+      </div>
+    );
+  }
+}
