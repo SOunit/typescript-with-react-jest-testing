792e0da6bbb7b01aa062cdad71bddc8a96753677
SOunit <workbookwork7@gmail.com>
2022-04-13 18:03:21 -0700

    add dynamic user attributes rendering

--- 03_advanced_project_features/src/services/AuthService.ts
+++ 03_advanced_project_features/src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/Model";
+import { User, UserAttribute } from "../models/Model";
 
 export class AuthService {
   public async login(
@@ -13,5 +13,16 @@ export class AuthService {
     } else {
       return undefined;
     }
+  }
+
+  public async getUserAttributes(user: User): Promise<UserAttribute[]> {
+    const result: UserAttribute[] = [];
+    result.push(
+      { Name: "description", Value: "Best user ever!!!" },
+      { Name: "job", Value: "engineer" },
+      { Name: "age", Value: "28" },
+      { Name: "experience", Value: "3 years" }
+    );
+    return result;
   }
 }
