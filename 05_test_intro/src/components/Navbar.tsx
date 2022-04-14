63b5d7e4047f6510589c8d646bd1290937be3de3
SOunit <workbookwork7@gmail.com>
2022-04-14 09:49:59 -0700

    add new project

---
+++ 05_test_intro/src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { User } from "../models/Model";
+
+export class Navbar extends React.Component<{ user: User | undefined }> {
+  render() {
+    let loginLogOut: any;
+    if (this.props.user) {
+      loginLogOut = (
+        <Link to="/logout" style={{ float: "right" }}>
+          {this.props.user.userName}
+        </Link>
+      );
+    } else {
+      loginLogOut = (
+        <Link to="/login" style={{ float: "right" }}>
+          login
+        </Link>
+      );
+    }
+
+    return (
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/spaces">Spaces</Link>
+        {loginLogOut}
+      </div>
+    );
+  }
+}
